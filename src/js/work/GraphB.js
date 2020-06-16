64afb0275a6d1490e3d409c39e6a209c722f0e74
HiramatsuChihiro <hiramatsu.chr@ncontr.com>
2020-06-16 20:02:17 +0900

    重複データ検討中

--- src/js/work/GraphB.js
+++ src/js/work/GraphB.js
@@ -9,13 +9,13 @@ export default () => {
   const chartData = React.useContext(ChartDataContext);
 
   return (
-    <div style={{ width: '100%', height: '300px' }}>
+    <div style={{ width: '600px', height: '300px' }}>
       <h4 style={{ color: "#333", margin:'0'}}>{chartData.chartTitle}</h4>
       <ResponsiveContainer height='80%'>
         <LineChart data={chartData.chartdata} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time" />
-          <YAxis />
+          <XAxis dataKey="time" allowDuplicatedCategory="false" />
+          <YAxis interval="preserveStartEnd"/>
           <Legend wrapperStyle={{ color: '#333' }} />
           <Line name={chartData.data1name} dataKey={chartData.data1key} stroke="#8884d8" />
           <Line name={chartData.data2name} dataKey={chartData.data2key} stroke="#82ca9d" />
